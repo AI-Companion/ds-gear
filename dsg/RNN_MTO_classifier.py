518a1027ea00e1e444823da0fa501b90e4932025
mmarouen <azzouz.marouen@gmail.com>
2020-12-28 14:31:13 +0100

    change architecture

--- dsg/RNN_MTO_classifier.py
+++ dsg/RNN_MTO_classifier.py
@@ -147,11 +147,10 @@ class RNNMTO(BaseRNN):
         input_layer = Input(shape=(self.max_length,), name='input')
         x = self.embedding_layer(input_layer)
         if self.n_labels == 2:
-            x = LSTM(16, dropout=0.2, recurrent_dropout=0.2, return_sequences=True)(x)
             x = LSTM(16, dropout=0.2, recurrent_dropout=0.2)(x)
-            x = Dense(4, activation='relu')(x)
-            x = BatchNormalization()(x)
-            x = Dropout(0.5)(x)
+            # x = Dense(4, activation='relu')(x)
+            # x = BatchNormalization()(x)
+            x = Dropout(0.25)(x)
             x = Dense(1, activation='sigmoid')(x)
         else:
             x = LSTM(64, dropout=0.2, recurrent_dropout=0.2)(x)
